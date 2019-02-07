dc97a2f5cfee30ebfd961f23e2e59ca62906ac95
shreya1798 <shreyasgnr@gmail.com>
2019-02-07 15:00:41 +0530

    tree added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
+import {MatExpansionModule, MatProgressBar, MatProgressBarModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,11 +11,18 @@ import { MaintaskComponent } from './maintask/maintask.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { ListComponent } from './list/list.component';
+import { TreeComponent } from './tree/tree.component';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, } from '@angular/material';
+import {MatTreeModule} from '@angular/material/tree';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     MaintaskComponent,
-    ListComponent
+    ListComponent,
+    TreeComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +31,12 @@ import { ListComponent } from './list/list.component';
     CoreModule,
     MatCardModule,
     MatExpansionModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatTreeModule,
+    MatIconModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
